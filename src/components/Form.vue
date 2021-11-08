b88ff4c70e191070a25201179e009a6a6c35fccc
Alexey Desyatnik <alexey.desyatnik@gmail.com>
2021-11-08 19:59:51 +0300

    Added personal data form elements

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="form"></div>
+  <div class="form">
+    <form action="">
+      <h2>Личные данные</h2>
+      <div class="row">
+        <label for="lastName" class="block">Фамилия</label>
+        <input id="lastName" />
+      </div>
+
+      <div class="row">
+        <label for="firstName" class="block">Имя</label>
+        <input id="lastName" />
+      </div>
+
+      <div class="row">
+        <label for="middleName" class="block">Отчество</label>
+        <input id="middleName" />
+      </div>
+
+      <div class="row">
+        <label for="birthDate" class="block">Дата рождения</label>
+        <input id="birthDate" placeholder="дд.мм.гггг" />
+      </div>
+
+      <div class="row">
+        <label for="email" class="block">E-mail</label>
+        <input id="email" />
+      </div>
+
+      <div class="row">
+        <label class="block">Пол</label>
+        <input type="radio" id="maleGender" name="gender" checked />
+        <label for="maleGender">Мужской</label>
+        <input type="radio" id="femaleGender" name="gender" />
+        <label for="maleGender">Женский</label>
+      </div>
+
+      <button>Отправить</button>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -12,6 +50,10 @@ export default {
 </script>
 
 <style scoped>
-.form {
+.row {
+  margin: 15px 0;
+}
+.block {
+  display: block;
 }
 </style>
