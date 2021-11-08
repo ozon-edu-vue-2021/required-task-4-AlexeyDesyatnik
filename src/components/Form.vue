cbbdc292059fa47730dcc676843badcba507329f
Alexey Desyatnik <alexey.desyatnik@gmail.com>
2021-11-08 20:11:59 +0300

    Added formData and v-bound it to input elements

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,38 +1,61 @@
 <template>
   <div class="form">
+    {{ formData }}
     <form action="">
       <h2>Личные данные</h2>
       <div class="row">
         <label for="lastName" class="block">Фамилия</label>
-        <input id="lastName" />
+        <input id="lastName" v-model="formData.lastName" />
       </div>
 
       <div class="row">
         <label for="firstName" class="block">Имя</label>
-        <input id="lastName" />
+        <input id="firstName" v-model="formData.firstName" />
       </div>
 
       <div class="row">
         <label for="middleName" class="block">Отчество</label>
-        <input id="middleName" />
+        <input id="middleName" v-model="formData.middleName" />
       </div>
 
       <div class="row">
         <label for="birthDate" class="block">Дата рождения</label>
-        <input id="birthDate" placeholder="дд.мм.гггг" />
+        <input
+          id="birthDate"
+          placeholder="дд.мм.гггг"
+          v-model="formData.birthDate"
+        />
       </div>
 
       <div class="row">
         <label for="email" class="block">E-mail</label>
-        <input id="email" />
+        <input id="email" v-model="formData.email" />
       </div>
 
       <div class="row">
         <label class="block">Пол</label>
-        <input type="radio" id="maleGender" name="gender" checked />
-        <label for="maleGender">Мужской</label>
-        <input type="radio" id="femaleGender" name="gender" />
-        <label for="maleGender">Женский</label>
+        <input
+          type="radio"
+          name="gender"
+          id="gender_male"
+          value="male"
+          v-model="formData.gender"
+        />
+        <label for="gender_male">Мужской</label>
+        <input
+          type="radio"
+          name="gender"
+          id="gender_female"
+          value="female"
+          v-model="formData.gender"
+        />
+        <label for="gender_female">Женский</label>
+      </div>
+
+      <h2>Паспортные данные</h2>
+      <div class="row">
+        <label for="nationality" class="block">Гражданство</label>
+        <input id="nationality" />
       </div>
 
       <button>Отправить</button>
@@ -43,7 +66,17 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      formData: {
+        lastName: '',
+        firstName: '',
+        middleName: '',
+        birthDate: '',
+        email: '',
+        gender: '',
+        nationality: '',
+      },
+    };
   },
   methods: {},
 };
@@ -51,7 +84,7 @@ export default {
 
 <style scoped>
 .row {
-  margin: 15px 0;
+  margin: 20px 0;
 }
 .block {
   display: block;
